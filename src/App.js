455ad4e6cf6483f4eca92837e6789708795ae76d
Mojtaba Maleki <mojtaba.maleki.138022@gmail.com>
2022-07-17 15:41:18 +0200

    Fun tailWindCss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Activity from './components/activities'
+ 
+function App() {
+  return (
+    
+    <div className="App bg-[url('https://picsum.photos/1920/1080')]">
+      <Activity/>
+    </div>
+  );
+}
+
+export default App;
